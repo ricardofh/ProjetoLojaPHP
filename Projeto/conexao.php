9ab4472d4ca874222973f4249fbf5c107c9e1505
Ricardo <ricardofh33@gmail.com>
2021-03-19 17:35:23 -0300

    Conexao com o Banco
    Inicio do projeto e conexao com o banco de dados

---
+++ Projeto/conexao.php
@@ -0,0 +1,11 @@
+<?php
+//requisição de conexão
+require_once('config.php');
+
+date_default_timezone_set ('America/Sao_Paulo');
+
+$pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8"
+    , "$usuario", "$senha");
+
+
+?>
