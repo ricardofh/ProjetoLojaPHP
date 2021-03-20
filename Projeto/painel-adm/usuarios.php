07cdc2280af3bf4309d49ea245b089f22ff258b4
Ricardo <ricardofh33@gmail.com>
2021-03-20 16:48:24 -0300

    Modal
    feat(usuarios): Adicionado botão de novo usuario e modal.
    feat(usuarios): Formulario de cadastro

--- Projeto/painel-adm/usuarios.php
+++ Projeto/painel-adm/usuarios.php
@@ -1,1 +1,44 @@
-USUARIO
+<?php 
+    $pag = 'usuarios';
+?>
+
+<a href="index.php?pagina=<?php echo $pag ?>&funcao=novo" type="button" class="btn btn-secondary mt-2">Novo Usuário</a>
+
+
+<!--MODAL -->
+<div class="modal fade" tabindex="-1" id="modalCadastrar">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Inserir Registro</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <!--FORMULARIO DE CADASTRO -->
+            <form method="POST">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Nome</label>
+                        <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome">
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                    <button name="btn-salvar" id="btn-salvar" type="button" class="btn btn-primary">Salvar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php
+    if(@$_GET['funcao'] == "novo"){ ?>
+        <script type="text/javascript">
+            var myModal = new bootstrap.Modal(document.getElementById('modalCadastrar'), {
+                backdrop: 'static'
+            })
+    
+            myModal.show();
+        </script>
+    <?php } ?>
