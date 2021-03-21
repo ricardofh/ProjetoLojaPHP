1b888e69fe510ee82f2df3d67d35c845c930ab88
Ricardo <ricardofh33@gmail.com>
2021-03-21 15:25:41 -0300

    Formulario de Usuario
    feat(usuarios): Adicionado o formulario de cadastro de usuarios, porém ainda não está funcionando

--- Projeto/painel-adm/usuarios.php
+++ Projeto/painel-adm/usuarios.php
@@ -1,5 +1,5 @@
-<?php 
-    $pag = 'usuarios';
+<?php
+$pag = 'usuarios';
 ?>
 
 <!-- BOOTSTRAP -->
@@ -14,7 +14,7 @@
 <div class="modal fade" tabindex="-1" id="modalCadastrar">
     <div class="modal-dialog">
         <div class="modal-content">
-            
+
             <div class="modal-header">
                 <h5 class="modal-title">Inserir Registro</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -22,20 +22,57 @@
 
             <!--FORMULARIO DE CADASTRO -->
             <form method="POST">
-                
+
                 <div class="modal-body">
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Nome</label>
-                        <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome">
+
+                    <!--NOME E CPF -->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Nome</label>
+                                <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">CPF</label>
+                                <input type="text" class="form-control" id="cpf" name="cpf" placeholder="CPF">
+                            </div>
+                        </div>
                     </div>
 
-                </div>
+                    <!--DEMAIS DADOS-->
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Senha</label>
+                        <input type="text" class="form-control" id="senha" name="senha" placeholder="Senha">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Nivel</label>
+                        <select class="form-select mt-1" aria-label="Default select example" name="nivel">
+							
+							<option <?php if(@$nivel_ed == 'Operador'){ ?> selected <?php } ?>  value="Operador">Operador</option>
+
+							<option <?php if(@$nivel_ed == 'Administrador'){ ?> selected <?php } ?>  value="Administrador">Administrador</option>
+							
+							<option <?php if(@$nivel_ed == 'Tesoureiro'){ ?> selected <?php } ?>  value="Tesoureiro">Tesoureiro</option>
+                        
+                        </select>
+                    </div>
                 
+                </div>
+
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
                     <button name="btn-salvar" id="btn-salvar" type="button" class="btn btn-primary">Salvar</button>
                 </div>
-            
+
             </form>
         </div>
     </div>
@@ -44,11 +81,11 @@
 
 <!-- CHAMANDO A MODEL -->
 <?php
-    if(@$_GET['funcao'] == "novo"){ ?>
-        <script type="text/javascript">
-            var myModal = new bootstrap.Modal(document.getElementById('modalCadastrar'));
-            myModal.show();
-        </script>
-    <?php } ?>
-
-    
+if (@$_GET['funcao'] == "novo") { ?>
+    <script type="text/javascript">
+        var myModal = new bootstrap.Modal(document.getElementById('modalCadastrar'), {
+            backdrop: 'static'
+        })
+        myModal.show();
+    </script>
+<?php } ?>
